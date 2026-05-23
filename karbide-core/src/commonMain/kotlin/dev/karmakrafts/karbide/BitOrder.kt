cb8903df09df7f1d892386e56b44fbc51dafe411
KitsuneAlex <alexh@karmakrafts.dev>
2026-05-23 21:27:58 +0200

    Add missing docs to BitOrder enum

--- karbide-core/src/commonMain/kotlin/dev/karmakrafts/karbide/BitOrder.kt
+++ karbide-core/src/commonMain/kotlin/dev/karmakrafts/karbide/BitOrder.kt
@@ -1,5 +1,18 @@
 package dev.karmakrafts.karbide
 
+/**
+ * In which order the bits of each byte are read/written from/to a source/sink.
+ */
 enum class BitOrder {
-    MSB_FIRST, LSB_FIRST
+    /**
+     * Most significant bit first.
+     * Reading/writing binary from left to right.
+     */
+    MSB_FIRST,
+
+    /**
+     * Least significant bit first.
+     * Reading/writing binary from right to left.
+     */
+    LSB_FIRST
 }
