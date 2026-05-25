2a14fb5601b98d7ba2be3f4b9fd9b77757ecc511
KitsuneAlex <alexh@karmakrafts.dev>
2026-05-25 04:04:51 +0200

    Optimize reverseBits on JVM/Android for 32- and 64-bit values

--- karbide-core/src/jvmAndAndroidMain/kotlin/dev/karmakrafts/karbide/BitUtils.kt
+++ karbide-core/src/jvmAndAndroidMain/kotlin/dev/karmakrafts/karbide/BitUtils.kt
@@ -1,13 +1,24 @@
-@file:JvmName("BitUtils$")
+@file:JvmName("BitUtils$") @file:Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
 
 package dev.karmakrafts.karbide
 
+import java.lang.Integer as JInt
+import java.lang.Long as JLong
+
 actual fun Byte.reverseBits(count: Int): Byte = reverseBitsCommon(count)
 actual fun Short.reverseBits(count: Int): Short = reverseBitsCommon(count)
-actual fun Int.reverseBits(count: Int): Int = reverseBitsCommon(count)
-actual fun Long.reverseBits(count: Int): Long = reverseBitsCommon(count)
+
+actual fun Int.reverseBits(count: Int): Int {
+    return if (count == Int.SIZE_BITS) JInt.reverse(this)
+    else reverseBitsCommon(count)
+}
+
+actual fun Long.reverseBits(count: Int): Long {
+    return if (count == Long.SIZE_BITS) JLong.reverse(this)
+    else reverseBitsCommon(count)
+}
 
 actual fun UByte.reverseBits(count: Int): UByte = reverseBitsCommon(count)
 actual fun UShort.reverseBits(count: Int): UShort = reverseBitsCommon(count)
-actual fun UInt.reverseBits(count: Int): UInt = reverseBitsCommon(count)
-actual fun ULong.reverseBits(count: Int): ULong = reverseBitsCommon(count)
+actual fun UInt.reverseBits(count: Int): UInt = toInt().reverseBits(count).toUInt()
+actual fun ULong.reverseBits(count: Int): ULong = toLong().reverseBits(count).toULong()
